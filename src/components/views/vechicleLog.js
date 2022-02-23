9371e05875c5c6a9b31d0e0d6bcde33fb356971a
Sai Charan <polavarapucharan@hotmail.com>
2022-02-23 12:28:49 +0530

    last

--- src/components/views/vechicleLog.js
+++ src/components/views/vechicleLog.js
@@ -10,99 +10,7 @@ import { useHistory } from 'react-router-dom'
 function VechicleLog() {
     const [value1, setValue1] = useState(null);
     const [dateRange, setDateRange] = useState('');
-    const [listData, setListData] = useState([
-        {
-        "id": 1,
-        "visitorName": null,
-        "ownerName": null,
-        "mobileNumber": null,
-        "vehicleNumber": "MP41BC0824",
-        "isActive": 1,
-        "department": null,
-        "building": null,
-        "isArchieved": 0,
-        "contactNumber": null,
-        "vehicleImage": "https://licenseimage.s3.ap-south-1.amazonaws.com/Mahindra-Bolero524259d.jpg_0001_0182_0251_0110_0031.png",
-        "vehicleColor": null,
-        "model": null,
-        "manfacturer": null,
-        "entryTime": "2022-02-14 14:00:00",
-        "exitTime": "2022-02-15 14:45:00"
-        },
-        {
-        "id": 2,
-        "visitorName": null,
-        "ownerName": null,
-        "mobileNumber": null,
-        "vehicleNumber": "MP41BC0824",
-        "isActive": 1,
-        "department": null,
-        "building": null,
-        "isArchieved": 0,
-        "contactNumber": null,
-        "vehicleImage": "https://licenseimage.s3.ap-south-1.amazonaws.com/Mahindra-Bolero524259d.jpg_0001_0182_0251_0110_0031.png",
-        "vehicleColor": null,
-        "model": null,
-        "manfacturer": null,
-        "entryTime": "2022-02-14 14:00:00",
-        "exitTime": "2022-02-15 14:45:00"
-        },
-        {
-        "id": 3,
-        "visitorName": null,
-        "ownerName": null,
-        "mobileNumber": null,
-        "vehicleNumber": "WB26K7597",
-        "isActive": 1,
-        "department": null,
-        "building": null,
-        "isArchieved": 0,
-        "contactNumber": null,
-        "vehicleImage": "https://licenseimage.s3.ap-south-1.amazonaws.com/Mahindra-Bolero527305c.jpg_0003_0515_0539_0189_0050.png",
-        "vehicleColor": null,
-        "model": null,
-        "manfacturer": null,
-        "entryTime": "2022-01-14 08:00:00",
-        "exitTime": "2022-01-15 14:45:00"
-        },
-        {
-        "id": 4,
-        "visitorName": null,
-        "ownerName": null,
-        "mobileNumber": null,
-        "vehicleNumber": "MH06BC4786",
-        "isActive": 1,
-        "department": null,
-        "building": null,
-        "isArchieved": 0,
-        "contactNumber": null,
-        "vehicleImage": "https://licenseimage.s3.ap-south-1.amazonaws.com/Mahindra-Bolero529017c.jpg_0000_0360_0344_0131_0045.png",
-        "vehicleColor": null,
-        "model": null,
-        "manfacturer": null,
-        "entryTime": "2021-08-10 08:00:00",
-        "exitTime": "2021-08-11 23:35:00"
-        },
-        {
-        "id": 5,
-        "visitorName": null,
-        "ownerName": null,
-        "mobileNumber": null,
-        "vehicleNumber": "TN03J0964",
-        "isActive": 1,
-        "department": null,
-        "building": null,
-        "isArchieved": 0,
-        "contactNumber": null,
-        "vehicleImage": "https://licenseimage.s3.ap-south-1.amazonaws.com/Mahindra-Bolero530318c.jpg_0000_0480_0445_0182_0042.png",
-        "vehicleColor": null,
-        "model": null,
-        "manfacturer": null,
-        "entryTime": "2021-09-09 11:00:00",
-        "exitTime": "2021-09-09 10:35:00"
-        }
-        ]
-       );
+    const [listData, setListData] = useState();
        const history = useHistory();
     const [filteredData, setFilteredData] = useState(listData);
     const vehicleImage = (rowData) => {
@@ -140,12 +48,14 @@ function VechicleLog() {
                 'Content-Type': 'application/json',
                 'Access-Control-Allow-Origin': '*',
                 'Access-Control-Allow-Methods': 'GET,PUT,POST,DELETE,PATCH,OPTIONS',
+                'Authorization': localStorage.getItem('token')
             },
         }).then(res=>{
-            setListData(res.data);
+            setListData(res.data.data);
+            setFilteredData(res.data.data);
         }
         ).catch(err=>{
-            setListData([]);
+            console.log(err);
         })
     }, []);
 
--- src/components/views/vechicleLogDetail.js
+++ src/components/views/vechicleLogDetail.js
@@ -1,105 +1,28 @@
 import { Column } from 'primereact/column';
 import { DataTable } from 'primereact/datatable';
-import React,{useEffect} from 'react'
+import React,{useEffect,useState} from 'react'
 import { useParams } from 'react-router-dom';
 import { Image } from 'primereact/image';
+import axios from 'axios';
 
 function VechicleLogDetail() {
     let { id } = useParams();
-    const data=[
-        {
-        "id": 1,
-        "visitorName": null,
-        "ownerName": null,
-        "mobileNumber": null,
-        "vehicleNumber": "MP41BC0824",
-        "isActive": 1,
-        "department": null,
-        "building": null,
-        "isArchieved": 0,
-        "contactNumber": null,
-        "vehicleImage": "https://licenseimage.s3.ap-south-1.amazonaws.com/Mahindra-Bolero524259d.jpg_0001_0182_0251_0110_0031.png",
-        "vehicleColor": null,
-        "model": null,
-        "manfacturer": null,
-        "entryTime": "2022-02-14 14:00:00",
-        "exitTime": "2022-02-15 14:45:00"
-        },
-        {
-        "id": 2,
-        "visitorName": null,
-        "ownerName": null,
-        "mobileNumber": null,
-        "vehicleNumber": "MP41BC0824",
-        "isActive": 1,
-        "department": null,
-        "building": null,
-        "isArchieved": 0,
-        "contactNumber": null,
-        "vehicleImage": "https://licenseimage.s3.ap-south-1.amazonaws.com/Mahindra-Bolero524259d.jpg_0001_0182_0251_0110_0031.png",
-        "vehicleColor": null,
-        "model": null,
-        "manfacturer": null,
-        "entryTime": "2022-02-14 14:00:00",
-        "exitTime": "2022-02-15 14:45:00"
-        },
-        {
-        "id": 3,
-        "visitorName": null,
-        "ownerName": null,
-        "mobileNumber": null,
-        "vehicleNumber": "WB26K7597",
-        "isActive": 1,
-        "department": null,
-        "building": null,
-        "isArchieved": 0,
-        "contactNumber": null,
-        "vehicleImage": "https://licenseimage.s3.ap-south-1.amazonaws.com/Mahindra-Bolero527305c.jpg_0003_0515_0539_0189_0050.png",
-        "vehicleColor": null,
-        "model": null,
-        "manfacturer": null,
-        "entryTime": "2022-01-14 08:00:00",
-        "exitTime": "2022-01-15 14:45:00"
-        },
-        {
-        "id": 4,
-        "visitorName": null,
-        "ownerName": null,
-        "mobileNumber": null,
-        "vehicleNumber": "MH06BC4786",
-        "isActive": 1,
-        "department": null,
-        "building": null,
-        "isArchieved": 0,
-        "contactNumber": null,
-        "vehicleImage": "https://licenseimage.s3.ap-south-1.amazonaws.com/Mahindra-Bolero529017c.jpg_0000_0360_0344_0131_0045.png",
-        "vehicleColor": null,
-        "model": null,
-        "manfacturer": null,
-        "entryTime": "2021-08-10 08:00:00",
-        "exitTime": "2021-08-11 23:35:00"
-        },
-        {
-        "id": 5,
-        "visitorName": null,
-        "ownerName": null,
-        "mobileNumber": null,
-        "vehicleNumber": "TN03J0964",
-        "isActive": 1,
-        "department": null,
-        "building": null,
-        "isArchieved": 0,
-        "contactNumber": null,
-        "vehicleImage": "https://licenseimage.s3.ap-south-1.amazonaws.com/Mahindra-Bolero530318c.jpg_0000_0480_0445_0182_0042.png",
-        "vehicleColor": null,
-        "model": null,
-        "manfacturer": null,
-        "entryTime": "2021-09-09 11:00:00",
-        "exitTime": "2021-09-09 10:35:00"
-        }
-        ]
+    const [data, setData] = useState([]);
     const [vechicleDetails, setVechicleDetails] = React.useState([]);
     useEffect(() => {
+      axios.get("http://anpr.pushpak.cloud:5001/visitor",{
+            headers: {
+                'Content-Type': 'application/json',
+                'Access-Control-Allow-Origin': '*',
+                'Access-Control-Allow-Methods': 'GET,PUT,POST,DELETE,PATCH,OPTIONS',
+                'Authorization': localStorage.getItem('token')
+            },
+        }).then(res=>{
+            setData(res.data.data);
+        }
+        ).catch(err=>{
+            console.log(err);
+        })
      const items =  data.filter(item => item.id === parseInt(id));
         setVechicleDetails(items);
     }, [data,id]);
